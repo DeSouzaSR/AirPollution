51158f19214d88d8d4f9966251a9735a3a966e01
DeSouzaSR <sandro.fisica@gmail.com>
2023-04-23 11:03:03 -0300

    The pollutantmean function has completed. Added the check_pollutantmean file to test the function.

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,18 +2,36 @@
 rm(list = ls())
 
 pollutantmean <- function(diretory, pollutant, id = 1:332){
-  # "directory" is a character vector of length 1 indicating the location
-  # of the CSV file.
+  # INPUT
+  #   - directory: a character vector of length 1 indicating the location of the
+  #     CSV file.
+  #   - pollutant: a character vector of length 1 indicating the name of the
+  #     pollutant for which we will calculate the mean, either "sulfate" or
+  #     "nitrate".
+  #   - id: an integer vector indicating the monitor ID numbers to be used
+  # OUTPUT
+  #   - Return de mean of the pollutant across all monitors list in the "id"
+  #     vectors, ignoring NA values.
   
-  # "pollutant" is a character vector of length 1 indicating the name
-  # of the pollutant for which we will calculate the mean, either "sulfate" orpo
-  # "nitrate".
+  # Format IDs
+  ids <- sprintf("%03d", id)
   
-  # "id" is an integer vector indicating the monitor ID numbers to be used
+  # Identify pollutant and make column of the classes
+  if(pollutant == "sulfate"){
+    str.colClasses <- c("NULL", "numeric", "NULL", "NULL")
+  } else {
+    str.colClasses <- c("NULL", "NULL", "numeric", "NULL")
+  }
   
-  # Return de mean of the pollutante across all monitors list in the "id"
-  # vectors, ignoring NA values.
-  id <- sprintf("%03d", id)
-  pollutant <- read.table(paste(diretory, id, ".csv", sep = ""))
-  pollutant
+  df = data.frame() # Empty data frame
+  for(id in ids){
+    df2 <- read.table(paste(diretory, id, ".csv", sep = ""),
+                    skip = 1,         
+                    header = FALSE, sep = ",",
+                    colClasses = str.colClasses)
+    
+    df <- rbind(df, df2)
+  }
+  
+  mean(df[,1], na.rm = TRUE)
 }
